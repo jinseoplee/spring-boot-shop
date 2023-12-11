a66a127f73321dc431ff61f1772fcf84580c8475
jinseoplee <jjjp9966@gmail.com>
2023-12-11 15:46:54 +0900

    create user entity

---
+++ src/main/java/com/ljs/shop/entity/User.java
@@ -0,0 +1,44 @@
+package com.ljs.shop.entity;
+
+import com.ljs.shop.constant.Role;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+@Entity
+public class User extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(unique = true)
+    private String email; // 이메일
+
+    private String password; // 비밀번호
+    private String name; // 이름
+    private String address; // 주소
+
+    @Enumerated(EnumType.STRING)
+    private Role role = Role.USER;
+
+    @Builder
+    public User(String email, String password, String name, String address) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.address = address;
+    }
+
+    /**
+     * 비밀번호를 암호화된 값으로 설정합니다.
+     *
+     * @param encodedPassword 암호화된 비밀번호
+     */
+    public void encodePassword(String encodedPassword) {
+        this.password = encodedPassword;
+    }
+}
