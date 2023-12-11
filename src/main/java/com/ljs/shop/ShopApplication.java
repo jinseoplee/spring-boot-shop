e935cd9e826974500da3cd54262d7b96bd369c70
jinseoplee <jjjp9966@gmail.com>
2023-12-11 15:55:31 +0900

    add @EnableJpaAuditing to ShopApplication

--- src/main/java/com/ljs/shop/ShopApplication.java
+++ src/main/java/com/ljs/shop/ShopApplication.java
@@ -2,12 +2,14 @@ package com.ljs.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // JPA Auditing 활성화
 @SpringBootApplication
 public class ShopApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ShopApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ShopApplication.class, args);
+    }
 
 }
