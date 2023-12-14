a15ca022f4f67f8612ba296a50bbef119ef32a2f
jinseoplee <jjjp9966@gmail.com>
2023-12-14 16:50:37 +0900

    implement signin functionality in UserService

--- src/main/java/com/ljs/shop/service/UserService.java
+++ src/main/java/com/ljs/shop/service/UserService.java
@@ -5,6 +5,9 @@ import com.ljs.shop.exception.DuplicateEmailException;
 import com.ljs.shop.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -12,7 +15,7 @@ import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
@@ -31,5 +34,13 @@ public class UserService {
 
         // 가입된 이메일이 없으면 회원가입
         return userRepository.save(user);
+    }
+
+    // 사용자 이메일을 기반으로 사용자 인증 정보를 로드하는 메서드
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .map(CustomUserDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException("존재하지 않는 회원입니다."));
     }
 }
