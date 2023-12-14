95d2ea0d8949fb12bb8f1fca6a6551e1c692697f
jinseoplee <jjjp9966@gmail.com>
2023-12-14 16:50:14 +0900

    create CustomUserDetails

---
+++ src/main/java/com/ljs/shop/service/CustomUserDetails.java
@@ -0,0 +1,57 @@
+package com.ljs.shop.service;
+
+import com.ljs.shop.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+/**
+ * CustomUserDetails 클래스는 사용자 역할 및 인증과 관련된 부분에 중점을 두고, JPA 관련 책임은 User 엔터티에 위임한다.
+ */
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + user.getRole().toString()));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public final User getUser() {
+        return user;
+    }
+}
