13833979c5aeca8899056e69541b11112ba63297
jinseoplee <jjjp9966@gmail.com>
2023-12-14 16:47:44 +0900

    create CustomAccessDeniedHandler

---
+++ src/main/java/com/ljs/shop/config/CustomAccessDeniedHandler.java
@@ -0,0 +1,21 @@
+package com.ljs.shop.config;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import java.io.IOException;
+
+/**
+ * 권한이 없는 요청 시 홈 페이지로 리다이렉션 처리.
+ */
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request,
+                       HttpServletResponse response,
+                       AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        response.sendRedirect("/");
+    }
+}
