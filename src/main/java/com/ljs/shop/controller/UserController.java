46773d4e6b2ee6140a3dfb9977b4caf2cddd455d
jinseoplee <jjjp9966@gmail.com>
2023-12-14 16:50:50 +0900

    implement signin functionality in UserController

--- src/main/java/com/ljs/shop/controller/UserController.java
+++ src/main/java/com/ljs/shop/controller/UserController.java
@@ -45,4 +45,17 @@ public class UserController {
         // 회원 가입이 성공하면 홈페이지로 리다이렉트
         return "redirect:/";
     }
+
+    // 로그인 페이지를 보여주는 메서드
+    @GetMapping("/signin")
+    public String showSignInForm() {
+        return "user/signInForm";
+    }
+
+    // 로그인 실패 시 모델에 에러 메시지를 추가하고 로그인 페이지를 보여주는 메서드
+    @GetMapping("/signin/error")
+    public String showSignInError(Model model) {
+        model.addAttribute("signInErrorMessage", "아이디 또는 비밀번호가 일치하지 않습니다.");
+        return "user/signInForm";
+    }
 }
