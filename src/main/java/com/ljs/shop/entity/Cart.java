75749e4fd7ffa942fb264c42fa63cfdfc5ffcc41
jinseoplee <jjjp9966@gmail.com>
2023-12-15 21:52:14 +0900

    create Cart entity

---
+++ src/main/java/com/ljs/shop/entity/Cart.java
@@ -0,0 +1,23 @@
+package com.ljs.shop.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+@Entity
+public class Cart extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "cart_id")
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Cart(User user) {
+        this.user = user;
+    }
+}
