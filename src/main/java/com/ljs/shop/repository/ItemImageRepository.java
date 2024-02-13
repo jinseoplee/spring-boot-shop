6ca0eea4307a02a8e8b81e0ef40524cc25359c1b
jinseoplee <jjjp9966@gmail.com>
2024-02-14 01:53:21 +0900

    feat: ItemImageRepository에 findByItemId 메서드 추가

--- src/main/java/com/ljs/shop/repository/ItemImageRepository.java
+++ src/main/java/com/ljs/shop/repository/ItemImageRepository.java
@@ -6,4 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ItemImageRepository extends JpaRepository<ItemImage, Long> {
+    /**
+     * 주어진 상품 ID에 해당하는 모든 상품 이미지를 조회한다.
+     *
+     * @param itemId 상품 ID
+     * @return 상품 이미지 목록
+     */
+    List<ItemImage> findByItemId(Long itemId);
 }
