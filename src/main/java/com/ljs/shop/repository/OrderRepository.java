f0c62dfceb7fb2554ded93a9a5c7b430e6551a0b
jinseoplee <jjjp9966@gmail.com>
2024-02-21 18:04:06 +0900

    feat: OrderRepository에 QuerydslPredicateExecutor 및 CustomOrderRepository 상속 추가

--- src/main/java/com/ljs/shop/repository/OrderRepository.java
+++ src/main/java/com/ljs/shop/repository/OrderRepository.java
@@ -2,6 +2,7 @@ package com.ljs.shop.repository;
 
 import com.ljs.shop.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface OrderRepository extends JpaRepository<Order, Long> {
+public interface OrderRepository extends JpaRepository<Order, Long>, QuerydslPredicateExecutor<Order>, CustomOrderRepository {
 }
